e3032aa0fdb3f45cde167abea06b2827c9eb1d6b
Ivan Santos <pragmaticivan@gmail.com>
2014-04-06 23:21:41 -0700

    Fix #39 - Fix installation breaks when Alfred and iTerm2 is not installed

--- Gruntfile.js
+++ Gruntfile.js
@@ -325,11 +325,17 @@ module.exports = function(grunt) {
             },
 
             theme_alfred: {
-                command: 'open <%= config.themes.path_dracula %>/alfred/Dracula.alfredappearance'
+                command: '[ -d "/Applications/Alfred 2.app" ] && open <%= config.themes.path_dracula %>/alfred/Dracula.alfredappearance || echo "Alfred App is not installed"',
+                options: {
+                     stdout: true
+                } 
             },
 
             theme_iterm: {
-                command: 'open <%= config.themes.path_dracula %>/iterm/Dracula.itermcolors'
+                command: '[ -d "/Applications/iTerm.app" ] && open <%= config.themes.path_dracula %>/iterm/Dracula.itermcolors || echo "iTerm App is not installed"',
+                options: {
+                     stdout: true
+                }
             },
 
             node_latest: {
